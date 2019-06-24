aea261f1cfe75425e113fa8caed00304c75b917d
timsuchanek <tim.suchanek@gmail.com>
2019-06-24 16:46:38 +0200

    update generator & prisma2

--- prisma/generator/index.js
+++ prisma/generator/index.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
 const fs = require('fs')
+const byline = require('./byline')
 
-process.stdin.on('data', chunk => {
-  const json = JSON.parse(String(chunk))
+byline(process.stdin).on('data', line => {
+  const json = JSON.parse(String(line))
   fs.writeFileSync('result.json', JSON.stringify(json, null, 2))
   process.exit(0)
 })
